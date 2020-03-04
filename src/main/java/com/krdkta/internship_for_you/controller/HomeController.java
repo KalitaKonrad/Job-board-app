5c662a84bfa14f11f80d1e2403152da5cd3a21d0
Konrad Kalita <konradkalita@student.agh.edu.pl>
2020-03-04 19:06:05 +0100

    Add package.json (+lock), do manual bundling with 'yarn watch'

--- src/main/java/com/krdkta/internship_for_you/controller/HomeController.java
+++ src/main/java/com/krdkta/internship_for_you/controller/HomeController.java
@@ -2,11 +2,12 @@ package com.krdkta.internship_for_you.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-  @GetMapping(value = "/")
+  @RequestMapping(value = "/")
   public String index() {
     return "index";
   }
