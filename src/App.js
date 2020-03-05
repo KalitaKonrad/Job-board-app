c8b9facac1c83e61d4f2f89b6147be5b7d9d2e02
Konrad Kalita <konradkalita@student.agh.edu.pl>
2020-03-05 10:22:07 +0100

    Refactor App to be class instead of a function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return <div></div>;
+class App extends Component {
+    ping = () => {
+        axios.get("http://localhost:8080/pong").then(res => {
+            alert('Received message from server!')
+        }).catch(err => {
+            alert('Something went wrong!')
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.ping}>Ping me!</button>
+            </div>
+        );
+    }
+
 }
 
 export default App;
