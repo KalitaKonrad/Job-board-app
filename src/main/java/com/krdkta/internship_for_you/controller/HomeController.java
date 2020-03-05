16ceeeb63ed6edc62450b5f149f5d34cbcf76b14
Konrad Kalita <konradkalita@student.agh.edu.pl>
2020-03-05 10:20:03 +0100

    Add test endpoint in HomeController, update pom.xml

--- src/main/java/com/krdkta/internship_for_you/controller/HomeController.java
+++ src/main/java/com/krdkta/internship_for_you/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.krdkta.internship_for_you.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@CrossOrigin(value = "*")
 public class HomeController {
 
-  @RequestMapping(value = "/")
+  @GetMapping(value = "/pong")
   public String index() {
     return "index";
   }
