06a489176b102978aa8c5a982e9d75fbdc64bec6
Konrad Kalita <konradkalita@student.agh.edu.pl>
2020-03-07 14:19:46 +0100

    Finish setting up redux -> Wrap App in Provider, setup REDUX DEVTOOLS

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import axios from './api/api_config';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { createOffer } from './actions/index';
 import Offer from './components/Offer';
 import Offers from './components/Offers';
+import { useSelector, useDispatch } from 'react-redux';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='container max-h-full max-w-full w-screen h-screen'>
-        <Navbar />
-        <Router>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/internship'>
-            <Offer />
-            <Offers />
-          </Route>
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  const offers = useSelector(state => state.offer);
+  const dispatch = useDispatch();
+
+  return (
+    <div className='container max-h-full max-w-full w-screen h-screen'>
+      <Navbar />
+      <Router>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/internship'>
+          <Offer />
+          <Offers />
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,10 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import './assets/main.css';
 import { createStore } from 'redux';
 import allReducers from './reducers/index';
+import { Provider } from 'react-redux';
 
-const store = createStore(allReducers);
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
